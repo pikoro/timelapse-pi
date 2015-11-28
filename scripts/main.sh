cdd0458ea3eabe6869a08af98d162b6bca3f8c36
pikoro <dccpikoro+github@gmail.com>
2015-11-27 23:05:18 -0500

    Update main.sh
    Added duration based loop to take pictures at interval specified in config file for duration based in config file.

--- scripts/main.sh
+++ scripts/main.sh
@@ -10,18 +10,24 @@ if [ $CFG_ENABLED != 1 ]; then
 		exit
 	fi
 fi 
-
-# Take pic
-bash ./take-picture.sh $CFG_IMAGE_DIR
-
-# Upload image directory
-for file in $CFG_FTP_UPLOAD_FILES
+let x=1
+while [ $x < $CFG_TIMELAPSE_DURATION ]
 do
-	echo "Working on...$(basename $file)"
-	bash ./ftp-file.sh $CFG_FTP_SERVER $CFG_FTP_USER $CFG_FTP_PASS $file
+	# Take pic
+	bash ./take-picture.sh $CFG_IMAGE_DIR
+
+	# Upload image directory
+	for file in $CFG_FTP_UPLOAD_FILES
+	do
+		echo "Working on...$(basename $file)"
+		bash ./ftp-file.sh $CFG_FTP_SERVER $CFG_FTP_USER $CFG_FTP_PASS $file
 	
-	# TODO: Check success of FTP before deleting
-	rm -f $file
+		# TODO: Check success of FTP before deleting
+		rm -f $file
+	done
+
+  sleep $CFG_IMAGE_INTERVAL
+  $x=$x + $CFG_IMAGE_INTERVAL
 done
 
 
